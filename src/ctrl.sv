887766ab588532f7c1f9be7a5bd0b0b153601853
Saad <46577833+saads312@users.noreply.github.com>
2025-07-27 10:35:13 -0400

    created alu and alu controller, working on main controller

---
+++ src/ctrl.sv
@@ -0,0 +1,17 @@
+module ctrl (
+    input [2:0] funct3,
+    input [6:0] funct7,
+    input [6:0] opcode,
+    input zero,
+    output pc_src,
+    output result_src,
+    output mem_write,
+    output [1:0] alu_op,
+    output alu_src,
+    output [1:0] imm_src,
+    output reg_write
+);
+
+
+    
+endmodule
